33b73d0d3b090b552b6ca70cff9ba56c1135c422
WebisBrian <brian.t.github@gmail.com>
2025-11-17 21:52:03 +0100

    Initialisation groupID adapté

--- src/main/java/com/webisbrian/hospital_bed_planner/Main.java
+++ src/main/java/com/webisbrian/hospital_bed_planner/Main.java
@@ -1,4 +1,4 @@
-package com.webisbrian;
+package com.webisbrian.hospital_bed_planner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
